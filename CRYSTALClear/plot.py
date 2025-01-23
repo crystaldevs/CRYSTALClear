7c6537f8ab0014277f76ced3039836a5bdcf4b76
Davide Mitoli <davidemitoli@gmail.com>
2025-01-23 11:23:50 +0100

    Fix plotting options for optional arguments in plot_cry_spec_multi()

--- CRYSTALClear/plot.py
+++ CRYSTALClear/plot.py
@@ -3602,11 +3602,14 @@ def plot_cry_spec(transitions, typeS, components=False, bwidth=5, stdev=3, eta=0
         exp_data[:, 1] = exp_data[:, 1] * norm_fac - baseline  # * 0.5
         plt.plot(exp_data[:, 0], exp_data[:, 1], 'r-', linewidth=linewidth)
 
-    if label is not None:
+    if ((label is not None) and (style is None)):
         plt.plot(spec_data[:, 0], spec_data[:, 1], linewidth=linewidth,
                  label=label)
-    elif (style is not None):
+    elif ((label is None) and (style is not None)):
         plt.plot(spec_data[:, 0], spec_data[:, 1], style, linewidth=linewidth)
+    elif ((label is not None) and (style is not None)):
+        plt.plot(spec_data[:, 0], spec_data[:, 1], style, linewidth=linewidth,
+                 label=label)
     else:
         plt.plot(spec_data[:, 0], spec_data[:, 1], linewidth=linewidth)
 
